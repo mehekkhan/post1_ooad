e03c1cf04e029dec19ab617f0101be0a254478ae
mehekkhan <mehekz@live.com>
2017-02-09 15:23:40 -0800

    Create Customer.java

---
+++ src/main/java/org/sfsu/post/views/Customer.java
@@ -0,0 +1,32 @@
+public class Customer {
+
+//Intance Fields
+public String firstName;
+public String lastName;
+public String fullName;
+
+//Constructor
+public Customer(String fullName){
+  this.fullName = fullName;
+  }
+  
+  public Customer(String firstName, String lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  
+  public Customer() { }
+  
+  //Accessor Methods
+  public String getFirstName(){
+    return firstName;
+   }
+   
+   public String getLastName(){
+    return lastName;
+   }
+   
+   public String getFullName(){
+    return fullName;
+   }
+ }
