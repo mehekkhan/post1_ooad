c2385e854e624eb161ce59ba5787b50d84793897
mehekkhan <mehekz@live.com>
2017-02-09 14:17:58 -0800

    Create Payment.java

---
+++ src/main/java/org/sfsu/post/controller/Payment.java
@@ -0,0 +1,20 @@
+public class Payment {
+	
+	//Instance fields
+	public String type;
+	public Float price;
+	
+	//Constructor
+	public Payment(String type, float price){
+		this.type= type;
+		this.price= price;
+	}
+	
+	//Accessor Methods
+	public String getType(){
+		return type;
+	}
+	public double getPrice(){
+		return price;
+	}
+}
